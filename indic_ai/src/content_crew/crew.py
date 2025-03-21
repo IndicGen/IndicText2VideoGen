ed6c0798b5142c3646d7436a398a5323e12c09b4
vamsimalineni96 <vamsimalineni6@gmail.com>
2025-03-21 16:26:32 +0530

    folder structure changes

---
+++ indic_ai/src/content_crew/crew.py
@@ -0,0 +1,35 @@
+# src/research_crew/crew.py
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from src.content_crew.tools.extract_tool import ExtractorTool
+import os
+
+@CrewBase
+class ContentCrew():
+    """Research crew for comprehensive topic analysis and reporting"""
+    
+    @agent
+    def temple_tour_guide_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['temple_tour_guide_agent'],
+            verbose=True,
+            llm=os.getenv("LLM_MODEL")
+            
+        )
+        
+    @task
+    def temple_tour_guide_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['temple_tour_guide_task'],
+            tools=[ExtractorTool()]
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the research crew"""
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+        )
---
+++ indic_ai/utils/name_extractor.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+from typing import List
+from utils.vectorstore import VectorStoreHandler
+from src.content_crew.config.configs import keywords
+
+vector_store_handler=VectorStoreHandler()
+
+def get_data(url)-> List[str]:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    data = {}
+    # Iterate over all headings (h1 to h6)
+    for header in soup.find_all(re.compile('^h[1-6]$')):
+        heading = header.get_text(strip=True)
+        content = ""
+        next_node = header.find_next_sibling()
+
+        # Collect content until the next heading
+        while next_node and not (next_node.name and re.match(r'h[1-6]', next_node.name)):
+            if next_node.name:
+                content += next_node.get_text(separator=" ", strip=True) + " "
+            next_node = next_node.find_next_sibling()
+
+        data[heading] = content.strip()
+    
+    # Filtering out the content present only on temples
+    temples={}
+    for key,value in data.items():
+        for word in keywords:
+            if word.lower() in key.lower() and "temples" not in key.lower():
+                key= re.sub(r"^\d+(\.\d+)?\.?\s*", "", key)
+                temples[key]=value
+                break
+    
+    # Uploading the details of the temples to chromadb
+    for temple_name, details in temples.items():
+        print(f"[DEBUG] Trying to upload the content about {temple_name} with length {len(details)}")
+        vector_store_handler.add_text(case_id=temple_name, text=details)
+    
+    temple_names=[]
+    for name,details in temples.items():
+        temple_names.append(name)
+    
+    return temple_names
